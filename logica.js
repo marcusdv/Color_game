abfc808d56bb1deb2f6b3307d2e6f604f08dff07
marcusdv <marcus.vinicius.bittencourt.c@gmail.com>
2020-03-05 22:18:34 -0300

    Algumas modificações na interface

--- logica.js
+++ logica.js
@@ -1,6 +1,6 @@
 var quadrados = document.querySelectorAll(".quadrado")
-var spanCor = document.querySelector("#cor-escolhida")
-var botoes = document.querySelectorAll("button")
+var spanRGB = document.querySelector("#cor-escolhida")
+var botoes = document.querySelectorAll(".botoes")
 var container = document.getElementById("container")
 var pontuacaoh1 = document.getElementById("pontuacaoH1")
 var quadradoNoDificil = document.getElementById("quadrados-no-dificil")
@@ -13,12 +13,13 @@ var corAleatoriaParaQuadrados
 var rgbCorCorreta
 var numeroDeQuadradosEmArrays = 2
 var dificuldadeDoJogo = "facil"
-var pontuacao = 0
+var pontuacao = 0 
 
 gerarDificuldade()
 // Gera cores aleatórias para o Array cores[]
 gerarCoresAleatorias()
 
+//Verifica se acertou, se sim pontua se não despontua
 for(var x = 0; x < quadrados.length; x++) {
     quadrados[x].addEventListener("click", function(){
       if(this.style.backgroundColor === rgbCorCorreta){
@@ -39,17 +40,17 @@ function geradorDeTonalidadeAleatoria() {
 
 function gerarCoresAleatorias() {      
     for(var x = 0; x <= numeroDeQuadradosEmArrays; x++) {
+        //Cria m RGB alepatorio pra cada quadrado
         corAleatoriaParaQuadrados = "rgb(" + geradorDeTonalidadeAleatoria() + ", " + geradorDeTonalidadeAleatoria() + ", " + geradorDeTonalidadeAleatoria() + ")"
         ArrayDeCores[x] = corAleatoriaParaQuadrados
-        console.log(corAleatoriaParaQuadrados)
         quadrados[x].style.backgroundColor = ArrayDeCores[x]
     }
+    //Gera a alternativa correta
     indexDaCorCorreta = Math.round(Math.random() * numeroDeQuadradosEmArrays);
     rgbCorCorreta = ArrayDeCores[indexDaCorCorreta]
-        //Coloca o rgb correto no H1
-    spanCor.textContent = ArrayDeCores[indexDaCorCorreta]
+    //Coloca o rgb correto no cabeçalho
+    spanRGB.textContent = ArrayDeCores[indexDaCorCorreta]
     nav.style.backgroundColor = ArrayDeCores[indexDaCorCorreta]
-    console.log(indexDaCorCorreta)
     pontuacaoH1.textContent = pontuacao
 }
 
@@ -61,6 +62,7 @@ function gerarDificuldade() {
                 dificuldadeDoJogo = "facil"
                 quadradoNoDificil.classList.add("invisivel")
                 quadradoNoOohShit.classList.add("invisivel")
+
             } else if (this.textContent === "Difícil") {
                 numeroDeQuadradosEmArrays = 5
                 dificuldadeDoJogo = "dificil"
@@ -72,9 +74,9 @@ function gerarDificuldade() {
                 quadradoNoDificil.classList.remove("invisivel")
                 quadradoNoOohShit.classList.remove("invisivel")
             }
-            console.log(numeroDeQuadradosEmArrays)
             pontuacao = 0
             gerarCoresAleatorias()
+
         })
     }
 }
@@ -86,12 +88,7 @@ function pontuar(){
         pontuacao += 3
     else if (dificuldadeDoJogo === "oohshit")
         pontuacao += 5  
-        
-    if (pontuacao >= 0) {
-        for(var x = 0; x < facePalm.length; x++){
-            facePalm[x].classList.add("invisivel")
-        }
-    } 
+    toggleFacepalm() 
 }
 
 function despontuar(){
@@ -100,11 +97,18 @@ function despontuar(){
     else if (dificuldadeDoJogo === "dificil")
         pontuacao -= 1
     else if (dificuldadeDoJogo === "oohshit")
-        pontuacao -= 2   
+        pontuacao -= 2  
+    toggleFacepalm()  
+}
 
+function toggleFacepalm() {
     if (pontuacao < 0) {
         for(var x = 0; x < facePalm.length; x++){
             facePalm[x].classList.remove("invisivel")
         }
-    }
+    } if (pontuacao >= 0) {
+        for(var x = 0; x < facePalm.length; x++){
+            facePalm[x].classList.add("invisivel")
+        }
+    } 
 }
